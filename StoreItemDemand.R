4afb29d5286c960a0dcd652aad167d279a08dea3
Ryan Wolff <ryan.h.wolff@gmail.com>
2023-11-15 16:37:21 -0700

    Random Forest for One Store-Item Combo

--- StoreItemDemand.R
+++ StoreItemDemand.R
@@ -74,6 +74,159 @@ acf_4_4 <- train[train$store == 4 & train$item == 4, ] %>%
 fourway_acf <- (acf_1_1 + acf_2_2) / (acf_3_3 + acf_4_4)
 fourway_acf
 
+#################################################################
+#################################################################
+# Feature Engineering for a Single Store-Item Combo #############
+#################################################################
+#################################################################
+
+### Load Data and Packages ###
+
+# Packages
+library(vroom)
+library(tidyverse)
+library(timetk)
+library(patchwork)
+library(tidyverse)
+library(embed)
+library(lubridate)
+library(parsnip)
+library(ranger)
+library(workflows)
+
+library(tidyverse)
+library(vroom)
+library(tidymodels)
+library(poissonreg)
+library(rpart)
+library(stacks)
+library(dbarts)
+library(xgboost)
+
+
+# Data
+train <- vroom("train.csv")
+test <- vroom("test.csv")
+
+# Subset store-item combo w my favorite numbers
+storeItem <- train %>%
+  filter(store == 4, item == 17)
+
+
+
+### Light EDA ###
+
+# Time Series Plot
+tsp <- storeItem %>%
+  plot_time_series(date, sales, .interactive = FALSE)
+
+# ACF (autocorrelation function plots)
+acf <- storeItem %>%
+  pull(sales) %>%
+  forecast::ggAcf(.) + ggtitle("ACF for Store 4 Item 4")
+
+# ACF-Year
+acf_y <- storeItem %>%
+  pull(sales) %>%
+  forecast::ggAcf(., lag.max = 365) + ggtitle("ACF for Store 4 Item 4")
+
+# ACF-All Time
+acf_all <- storeItem %>%
+  pull(sales) %>%
+  forecast::ggAcf(., lag.max = 5*365) + ggtitle("ACF for Store 4 Item 4")
+
+# Four-Way Plot
+fourway <- (tsp + acf) / (acf_y + acf_all)
+fourway
+
+# EDA Findings:
+# General increase in sales
+# Heavy weekly autocorrelation (7 days)
+# Biggest sales in the summer
+# Smallest sales in the winter
+
+
+
+### Recipe ###
+
+# Create Recipe
+rec <- recipe(sales ~ ., data = storeItem) %>%
+  step_rm(c('store', 'item')) %>%
+  step_date(date, features = c('week', 'month', 'quarter')) %>%
+  step_mutate_at(all_integer_predictors(), fn = factor) %>%
+  step_mutate(season = factor(case_when(
+    between(month(date), 3, 5) ~ "Spring",
+    between(month(date), 6, 8) ~ "Summer",
+    between(month(date), 9, 11) ~ "Fall",
+    TRUE ~ "Winter"
+  ))) %>%
+  step_mutate(cumulative_sales = cumsum(sales)) %>%
+  step_dummy(all_nominal_predictors()) # Make nominal predictors into dummy variables
+
+# Prep, Bake, and View Recipe
+prepped <- prep(rec)
+bake(prepped, storeItem)
+
+
+
+### Model: Random Forest ###
+
+# Model
+rf_model <- rand_forest(mtry = tune(),
+                              min_n = tune(),
+                              trees = 500) %>% # Type of Model
+  set_engine("ranger") %>% # What R function to use
+  set_mode("regression")
+
+
+# Workflow
+rf_workflow <- workflow() %>%
+  add_recipe(rec) %>%
+  add_model(rf_model)
+
+# Tuning grid
+tuning_grid <- grid_regular(mtry(range = c(1, 7)), # Grid of values to tune over
+                            min_n(),
+                            levels = 5) # levels = L means L^2 total tuning possibilities
+
+folds <- vfold_cv(storeItem, # Split data for CV
+                  v = 5, # 5 folds
+                  repeats = 1)
+
+# Run CV
+cv_results <- rf_workflow %>%
+  tune_grid(resamples = folds,
+            grid = tuning_grid,
+            metrics = metric_set(smape))
+
+# Find Best Tuning Parameters
+best_tune <- cv_results %>%
+  select_best("smape")
+best_tune
+# mtry = 7, min_n = 2
+
+cv_results %>% collect_metrics() %>%
+  filter(.metric == "smape")
+# mean for best_tune is 18.6
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # For later use (making predictions)
 ####################################################
